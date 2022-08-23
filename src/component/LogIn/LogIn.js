7f715b99ed3971d8c4adf4e703662aa8e740e8e2
DOANXUANVIEN <dxvien.19i@cit.udn.vn>
2022-08-23 17:08:04 +0700

    update

--- src/component/LogIn/LogIn.js
+++ src/component/LogIn/LogIn.js
@@ -24,7 +24,6 @@ const LogIn = () => {
         console.log(results.data);
         alert("Success Login");
       })
-      
       .catch((error) => {
         console.log(error);
         console.log(error.response);
